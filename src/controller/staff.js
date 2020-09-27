61b7dc40a8fc7781e4685f48b2a8fe9766b08571
xingjinhao <m18206380582_1@163.com>
2020-09-27 15:41:17 +0800

    新增门店信息管理、人员管理

---
+++ src/controller/staff.js
@@ -0,0 +1,285 @@
+/**
+ @Name：  大数据云赋能
+ @Author：Yu.Guo
+ */
+
+layui.define(['table', 'form'], function (exports) {
+    var $ = layui.$,
+        admin = layui.admin,
+        view = layui.view,
+        table = layui.table,
+        form = layui.form;
+
+    /*列表*/
+    table.render({
+        elem: '#LAY-staff-manage',
+        url: layui.setter.ajaxUrl + '/api/admin/cloud/report/list',
+        method: 'get',
+        cols: [
+            [{
+                type: 'checkbox'
+            }, {
+                type: 'numbers',
+                title: '序号',
+                align: 'center'
+            }, {
+                field: 'name',
+                title: '姓名',
+                align: 'center'
+            }, {
+                field: 'phone',
+                title: '手机号',
+                align: 'center'
+            }, {
+                field: 'character',
+                title: '角色',
+                align: 'center'
+            }, {
+                field: 'state',
+                title: '状态',
+                align: 'center'
+            }, {
+                field: 'createTime',
+                title: '注册时间',
+                align: 'center'
+            }, {
+                title: '操作',
+                align: 'center',
+                toolbar: '#table-cloud-operation',
+                width: 200
+            }]
+        ],
+        page: true,
+        text: {
+            none: '暂无数据',
+            error: '对不起，加载出现异常！'
+        },
+        done: function (data) {
+            if (data.code == 403) {
+                layer.closeAll();
+                admin.exit();
+                setTimeout(function () {
+                    layer.alert('此账号已在别处登录,请重新登录！', {
+                        icon: 5
+                    });
+                }, 666);
+            }
+        }
+    });
+
+    /*监听上下架开关*/
+    form.on('switch(switchTest)', function (data) {
+        var flag = this.checked;
+        var id = data.elem.attributes['switch_id'].nodeValue;
+        $.ajax({
+            url: layui.setter.ajaxUrl + '/api/admin/banner/upper',
+            dataType: 'JSON',
+            contentType: 'application/json;charset=UTF-8',
+            type: 'POST',
+            data: JSON.stringify({
+                token: layui.data('data').token,
+                'id': id
+            }),
+            success: function (data) {
+                if (data.data == 1) {
+                    if (flag == false) {
+                        layer.alert('已禁用', {
+                            icon: 1
+                        });
+                    } else {
+                        layer.alert('已启用', {
+                            icon: 1
+                        });
+                    }
+                } else {
+                    layer.alert('操作失败,请稍后重试', {
+                        icon: 2
+                    });
+                }
+                if (data.code == 403) {
+                    layer.closeAll();
+                    admin.exit();
+                    setTimeout(function () {
+                        layer.alert('此账号已在别处登录,请重新登录！', {
+                            icon: 5
+                        });
+                    }, 666);
+                }
+                $('checkbox').prop('checked', false);
+                layui.table.reload('LAY-banner-manage');
+            },
+            error: function (err) {
+                layer.alert("操作失败", {
+                    icon: 2
+                });
+                layui.table.reload('LAY-banner-manage');
+            }
+        });
+    });
+
+    /*监听数据报告编辑、删除操作*/
+    table.on('tool(LAY-staff-manage)', function (obj) {
+        var data = obj.data;
+        if (obj.event === 'del') {
+            layer.confirm('真的删除行么', function (index) {
+                $.ajax({
+                    url: layui.setter.ajaxUrl + "/api/admin/cloud/report/del",
+                    method: "post",
+                    contentType: 'application/json;charset=UTF-8',
+                    data: JSON.stringify({
+                        'token': layui.data('data').token,
+                        'id': obj.data.id
+                    }),
+                    success: function (data) {
+                        if (data.code == 403) {
+                            layer.closeAll();
+                            admin.exit();
+                            setTimeout(function () {
+                                layer.alert('此账号已在别处登录,请重新登录！', {
+                                    icon: 5
+                                });
+                            }, 666);
+                        }
+                        if (data.data == 1) {
+                            layer.alert("已删除", {
+                                icon: 1
+                            });
+                            layui.table.reload('LAY-staff-manage');
+                        } else {
+                            layer.alert("删除失败,请稍后重试", {
+                                icon: 2
+                            });
+                        }
+                    },
+                    error: function (data) {
+                        layer.alert("操作失败", {
+                            icon: 2
+                        });
+                    }
+                });
+                layer.close(index);
+            });
+        } else if (obj.event === 'edit') {
+            admin.popup({
+                title: '编辑',
+                area: ['600px', '650px'],
+                id: 'LAY-popup-user-add',
+                success: function (layero, index) {
+                    view(this.id).render('store/form', data).done(function () {
+                        form.on('submit(LAY-reportData-front-submit)', function (data) {
+                            var field = data.field;
+                            field.token = layui.data('data').token;
+                            field.id = obj.data.id;
+                            var id = '';
+                            $("input:checkbox[name='checkbox']:checked").each(function (i) {
+                                id += $(this).attr('id') + ',';
+                            });
+                            id = id.slice(0, -1);
+                            $.ajax({
+                                url: layui.setter.ajaxUrl + "/api/admin/cloud/report/edit",
+                                method: "post",
+                                contentType: 'application/json;charset=UTF-8',
+                                data: JSON.stringify({
+                                    field
+                                }),
+                                success: function (data) {
+                                    if (data.data == 1) {
+                                        layer.alert('已更新', {
+                                            icon: 1
+                                        });
+                                        layui.table.reload('LAY-store-manage');
+                                    } else {
+                                        layer.alert('编辑失败，请稍后重试', {
+                                            icon: 2
+                                        });
+                                    }
+                                    if (data.code == 403) {
+                                        layer.closeAll();
+                                        admin.exit();
+                                        setTimeout(function () {
+                                            layer.alert('此账号已在别处登录,请重新登录！', {
+                                                icon: 5
+                                            });
+                                        }, 10);
+                                    }
+                                },
+                                error: function (data) {
+                                    layer.alert('编辑失败，请稍后重试', {
+                                        icon: 2
+                                    });
+                                }
+                            });
+                            layer.close(index);
+                        });
+                    });
+                }
+            });
+        } else if (obj.event === 'detail') {
+            admin.popup({
+                title: '详情',
+                area: ['1000px', '700px'],
+                id: 'LAY-popup-user-add',
+                success: function (layero, index) {
+                    view(this.id).render('staff/detail', data).done(function () {
+                        table.render({
+                            url: layui.setter.ajaxUrl + '/'
+                            , elem: '#LAY-partner-manage'
+                            , method: "get"
+                            , contentType: 'application/json;charset=UTF-8'
+                            , where: {'token': layui.data('data').token}
+                            , title: '详情'
+                            , cols: [[
+                                {type: 'numbers',title: '序号',align: 'center'},
+                                {field: 'type', title: '类别', align: 'center'},
+                                {field: 'partnerName',title: '公司名称',align: 'center'},
+                                {field: 'liableName',title: '姓名',align: 'center'},
+                                {field: 'liablePhone',title: '手机号',align: 'center'},
+                                {field: 'address',title: '地址',align: 'center'},
+                                {field: 'status', title: '状态', align: 'center', templet: '#status' }
+                            ]]
+                            , text: {none: '暂无数据', error: '对不起，加载出现异常！'}
+                            , done: function (data) {
+                                if (data.code == 403) {
+                                    layer.closeAll();
+                                    admin.exit();
+                                    setTimeout(function () {
+                                        layer.alert('此账号已在别处登录,请重新登录！', {icon: 5});
+                                    }, 10);
+                                }
+                            }
+                        });
+
+                        table.render({
+                            url: layui.setter.ajaxUrl + '/'
+                            , elem: '#LAY-user-message'
+                            , method: "get"
+                            , contentType: 'application/json;charset=UTF-8'
+                            , where: {'token': layui.data('data').token}
+                            , title: '人员列表'
+                            , cols: [[
+                                {type: 'numbers',title: '序号',align: 'center'},
+                                {field: 'actualName', title: '姓名', align: 'center'},
+                                {field: 'phone', title: '手机号', align: 'center'},
+                                {field: 'role', title: '角色', align: 'center'},
+                                {field: 'status', title: '状态', align: 'center', templet: '#status' }
+                            ]]
+                            , text: {none: '暂无数据', error: '对不起，加载出现异常！'}
+                            , done: function (data) {
+                                if (data.code == 403) {
+                                    layer.closeAll();
+                                    admin.exit();
+                                    setTimeout(function () {
+                                        layer.alert('此账号已在别处登录,请重新登录！', {icon: 5});
+                                    }, 10);
+                                }
+                            }
+                        });
+                    });
+                }
+            });
+        }
+
+    });
+
+    exports('staff', {})
+});
--- start/json/menu.js
+++ start/json/menu.js
@@ -1,135 +1,77 @@
 {
-    "code" : 200
-    ,"msg" : "success"
-    ,"data":
-    [{
-        "name": "service_store"
-        , "title": "服务门店管理(平台)"
-        , "icon": "layui-icon-print"
-        , "list": [ {
-            "name": "service_store"
-            , "title": "门店列表"
-            , "jump": "serviceStore/list"
+    "code": 200,
+    "msg": "success",
+    "data": [{
+        "name": "service_store",
+        "title": "服务门店管理(平台)",
+        "icon": "layui-icon-print",
+        "list": [{
+            "name": "service_store",
+            "title": "门店列表",
+            "jump": "serviceStore/list"
         }]
     }, {
-        "title": "基础设置(平台)"
-        , "icon": "layui-icon-set"
-        , "list": [{
-            "title": "用户协议管理"
-            , "jump": "set/user_rule"
-        },{
-            "title": "关于我们"
-            , "jump": "set/about_us"
-        },{
-            "title": "门店到期提醒列表"
-            , "jump": "set/store_list"
-        },{
-            "title": "二维码管理"
-            , "jump": "set/or_code"
-        }]
-    } , {
-        "title": "轮播图管理"
-        , "icon": "layui-icon-carousel"
-        , "list": [{
-            "title": "轮播图列表"
-            , "jump": "banner/list"
-        }]
-    }, {
-        "name": "adminauth"
-        , "title": "权限管理"
-        , "icon": "layui-icon-console"
-        , "list": [{
-            "name": "adminauth"
-            , "title": "管理员管理"
-            , "jump": "adminauth/adminuser/list"
+        "title": "基础设置(平台)",
+        "icon": "layui-icon-set",
+        "list": [{
+            "title": "用户协议管理",
+            "jump": "set/user_rule"
         }, {
-            "name": "adminauth"
-            , "title": "角色管理"
-            , "jump": "adminauth/role/list"
-        }]
-    }, {
-        "name": "information"
-        , "title": "资讯管理"
-        , "icon": "layui-icon-chat"
-        , "list": [{
-            "name": "information_list"
-            , "title": "资讯列表"
-            , "jump": "information/list"
-        }]
-    }, {
-        "name": "register"
-        , "title": "赋能咨询登记管理"
-        , "icon": "layui-icon-service"
-        , "list": [{
-            "name": "register_list"
-            , "title": "登记列表"
-            , "jump": "register/list"
-        }]
-    }, {
-        "name": "enabling"
-        , "title": "赋能需求管理"
-        , "icon": "layui-icon-tabs"
-        , "list": [{
-            "name": "enabling_list"
-            , "title": "需求列表"
-            , "jump": "enabling/list"
-        }]
-    }, {
-        "name": "data_statistics"
-        , "title": "数据统计"
-        , "icon": "layui-icon-form"
-        , "list": [{
-            "name": "collection_number"
-            , "title": "采集企业数据量"
-            , "jump": "data/collection_number"
+            "title": "关于我们",
+            "jump": "set/about_us"
         }, {
-            "name": "cooperation_number"
-            , "title": "合作机构数量"
-            , "jump": "data/cooperation_number"
+            "title": "门店到期提醒列表",
+            "jump": "set/store_list"
         }, {
-            "name": "report_number"
-            , "title": "赋能报告总数量"
-            , "jump": "data/report_number"
+            "title": "二维码管理",
+            "jump": "set/or_code"
         }]
     }, {
-        "name": "users"
-        , "title": "用户管理"
-        , "icon": "layui-icon-user"
-        , "list": [{
-            "name": "user_list"
-            , "title": "用户列表"
-            , "jump": "user/user/list"
+        "title": "轮播图管理",
+        "icon": "layui-icon-carousel",
+        "list": [{
+            "title": "轮播图列表",
+            "jump": "banner/list"
         }]
     }, {
-        "name": "partner"
-        , "title": "合作机构管理"
-        , "icon": "layui-icon-transfer"
-        , "list": [{
-            "name": "partner_list"
-            , "title": "合作机构列表"
-            , "jump": "partner/list"
+        "name": "adminauth",
+        "title": "权限管理",
+        "icon": "layui-icon-console",
+        "list": [{
+            "name": "adminauth",
+            "title": "管理员管理",
+            "jump": "adminauth/adminuser/list"
+        }, {
+            "name": "adminauth",
+            "title": "角色管理",
+            "jump": "adminauth/role/list"
         }]
     }, {
-        "name": "modular_manager"
-        , "title": "模块管理"
-        , "icon": "layui-icon-template-1"
-        , "list": [{
-            "name": "modular_list"
-            , "title": "模块列表"
-            , "jump": "modular/list"
+        "name": "store",
+        "title": "信息管理(门店)",
+        "icon": "layui-icon-cols",
+        "list": [{
+            "name": "store",
+            "title": "信息管理",
+            "jump": "store/admin"
         }]
     }, {
-        "name": "industry_class_set"
-        , "title": "行业分类设置"
-        , "icon": "layui-icon-set"
-        , "list": [{
-            "name": "industry_list"
-            , "title": "行业列表"
-            , "jump": "industry/list"
+        "name": "adminauth",
+        "title": "合作商管理(门店)",
+        "icon": "layui-icon-group",
+        "list": [{
+            "name": "official_partner",
+            "title": "合作商管理",
+            "jump": "officialPartner/admin"
+        }]
+    }, {
+        "name": "adminauth",
+        "title": "人员管理(门店)",
+        "icon": "layui-icon-component",
+        "list": [{
+            "name": "staff",
+            "title": "人员管理",
+            "jump": "staff/admin"
         }]
     }]
 }
-
-
-
-
